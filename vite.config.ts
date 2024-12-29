a125a44074b068a6332e82a05ab61470375565a5
jonpena <gran_200014@hotmail.com>
2024-12-29 16:56:45 +0100

    docs: :wrench: config deploy app

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["@react-pdf/renderer"],
-  },
+  base: "./",
   build: {
     commonjsOptions: {
-      include: [/@react-pdf.*/, /node_modules/],
+      include: [/node_modules/],
     },
   },
 });
